caf0b9e943d90bb0d415c679f317e4ed8bfc2130
mongaless <hitstalker7@gmail.com>
2018-03-01 06:36:06 +0000

    Assets compiling - 3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :delete]
   
 	helper_method :sort_column, :sort_direction
 	
